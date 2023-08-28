50197ca323be9bc65509726740a72c4623490ea4
Shimmayyyy <danicarose1814@gmail.com>
2023-08-28 23:26:18 +0800

    fix householdbeneficiary

---
+++ src/app/template/home/pages/householdbeneficiary/householdbeneficiary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-householdbeneficiary',
+  templateUrl: './householdbeneficiary.component.html',
+  styleUrls: ['./householdbeneficiary.component.scss']
+})
+export class HouseholdbeneficiaryComponent {
+
+}
