2e0e52d354f122793df97fd54778935d59f16f6f
Kuya Git <marklowelmontealto14@gmail.com>
2023-11-04 21:41:36 +0800

    done analytics

--- src/app/template/home/pages/dashboard/dashboard.component.ts
+++ src/app/template/home/pages/dashboard/dashboard.component.ts
@@ -1,32 +1,124 @@
-import { Component, inject, OnInit, ViewChild } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 import { AnalyticsService } from 'src/app/services/analytics.service';
-import { Subscription } from 'rxjs';
-
-
+import { Subscription, forkJoin } from 'rxjs';
+import { Chart, registerables } from 'node_modules/chart.js'
+import { getalluser } from 'src/app/services/data';
+import { MatTableDataSource } from '@angular/material/table';
+import { DataService } from 'src/app/services/data.service';
+Chart.register(...registerables)
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit{
-  private breakpointObserver = inject(BreakpointObserver);
-
-  private subs_get_4psholder: Subscription = new Subscription();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  displayedColumns: string[] = [
+    'accountsdetails_id',
+    'name',
+    'mobile_number',
+    'address',
+    'account_type',
+  ];
+  alluserData = new MatTableDataSource<getalluser>([]);
   constructor(
-    private _analytics: AnalyticsService
+    private _analytics: AnalyticsService,
+    private _dataService: DataService,
   ) { }
   ngOnInit(): void {
-  this.getallStatuscount()
-  this.getStatus5()
-  this.get4psholderS()
-  this.getStatus4()
-  this.get4psholderS()
-  this.getallworking()
+    this.combinedSubscription()
+    this.getallworking()
+    this.get4psholderS()
+    this.getStatus4()
+    this.getallUser()
+    this.getallstatussum()
 }
+combinedSubscription() {
+  const allStatusCount$ = this._analytics.allstatuscount();
+  const status5$ = this._analytics.status5();
+
+  forkJoin([allStatusCount$, status5$]).subscribe(
+    ([allStatusCountResult, status5Result]) => {
+      this.allstatusCount = allStatusCountResult.result[0].analytics_allstatuscount;
+      this.beneficiaries = parseInt(this.allstatusCount);
+
+      this.status5 = status5Result.result[0].analytics_status5;
+      this.status5parse = parseInt(this.status5);
+
+      this.renderPieChart();
+    },
+    (error) => {
+      // Handle errors
+      console.log(error);
+    }
+  );
+}
+
+private get_all_status_sum : Subscription = new Subscription();
+countValues : any
+allstatusSum: any
+linechart: any;
+renderLineChart() {
+  this.linechart = new Chart("lineChart", {
+    type: 'bar',
+    data: {
+      labels: ['Status 1','Status 2','Status 3','Status 4','Status 5','Status 6','Status 7','Status 8','Status 9','Status 10','Status 11','Status 12'],
+      datasets: [{
+        label: 'Count of Status that already approved by school',
+        data: [],
+        backgroundColor:["#ea5545", "#f46a9b", "#ef9b20", "#edbf33", "#ede15b", "#bdcf32", "#87bc45", "#27aeef", "#b33dc6"]
+
+
+      }]
+    }
+  })
+}
+
+getallstatussum() {
+  this.get_all_status_sum.add(
+    this._analytics.allstatussum().subscribe(
+      (result) => {
+        this.allstatusSum = result.result[0];
+        this.countValues = Object.values(this.allstatusSum);
+        this.renderLineChart()
+        if (this.linechart) {
+          console.log(this.countValues)
+          // Update the data property of the chart with the extracted data
+          console.log(this.linechart)
+          this.linechart.data.datasets[0].data = this.countValues;
+          this.linechart.update();
+        }
+      }
+    )
+  );
+}
+
+
+public mychart : any
+renderPieChart() {
+  this.mychart = new Chart("pieChart", {
+    type: 'pie',
+    data: {
+      labels: ['Continue College', 'Beneficiaries'],
+      datasets: [{
+        label: 'Number of Beneficiaries',
+        data: [this.beneficiaries, this.status5parse],
+        borderWidth: 1,
+        hoverOffset: 4
+      }]
+    },
+  })
+}
+
+
+
 FourpsHolder:any = []
 get4psholderS() {
   this.subs_get_4psholder.add(
@@ -51,7 +143,7 @@ getStatus4() {
 }
 
 allstatusCount : any = [];
-beneficiaries : any = [];
+public beneficiaries : any = [];
 getallStatuscount() {
   this.subs_get_4psholder.add(
     this._analytics.allstatuscount()
@@ -61,12 +153,11 @@ getallStatuscount() {
         this.beneficiaries = parseInt(this.allstatusCount)
       }
     )
-
   )
 }
-
+private subs_get_4psholder: Subscription = new Subscription();
 status5: any = []
-status5parse: any = []
+public status5parse: any = []
 getStatus5() {
   this.subs_get_4psholder.add(
     this._analytics.status5()
@@ -74,20 +165,9 @@ getStatus5() {
       (result) => {
             this.status5 = result.result[0].analytics_status5;
             this.status5parse = parseInt(this.status5);
-            console.log(this.status5parse)
-            this.generatePie()
       }
     )
   )
-
-}
-
-
-generatePie() {
-  const sh = this.status5parse;
-  const benef = this.beneficiaries;
-  this.pieChartData.datasets[0].data = [sh, benef]
-  console.log(this.pieChartData)
 }
 
 
@@ -95,22 +175,31 @@ allworking: any = [];
 senior!: number;
 junior!: number;
 college!: number;
+public chartPie : any;
 
-
+private sub_get_allworking : Subscription = new Subscription();
 chartdata : any = []
 getallworking() {
-  this.subs_get_4psholder.add(
+  this.sub_get_allworking.add(
     this._analytics.allworking().subscribe((result) => {
       this.allworking = result.result;
       this.senior = parseInt(this.allworking[0].count_8);
       this.junior = parseInt(this.allworking[0].count_12);
       this.college = parseInt(this.allworking[0].count_10);
-      console.log(this.senior, this.college, this.junior);
-      // Update the barChartData dataset with the received data
       this.allworkingdata.datasets[0].data = [this.junior, this.senior, this.college];
+
+      // After data retrieval, call a function to render the bar chart
+      this.renderBarChart();
     })
   );
 }
+
+renderBarChart() {
+  if (this.chart && this.chart.chart) {
+    this.chart.chart.update();
+  }
+}
+
 
 public allworkingdata: ChartData<'bar'> = {
   labels: ['Junior Highschool Graduate', 'Senior Highschool Graduate',  'College Graduate'],
@@ -119,11 +208,8 @@ public allworkingdata: ChartData<'bar'> = {
       data: [],
       label: 'Beneficiaries Currently Working',
       backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-        'rgba(255, 205, 86, 0.2)',
+        'rgb(105,92,254)',
       ],
-      borderWidth: 1,
     },
   ],
 };
@@ -178,31 +264,56 @@ public allworkingdata: ChartData<'bar'> = {
   }): void {
     console.log(event, active);
   }
+  private subsription_get_all_user: Subscription = new Subscription();
 
-  public pieChartOptions: ChartConfiguration['options'] = {
-    responsive: true,
-    plugins: {
-      legend: {
-        display: true,
-        position: 'top',
-      },
-      datalabels: {
-        formatter: (value: any, ctx: any) => {
-          if (ctx.chart.data.labels) {
-            return ctx.chart.data.labels[ctx.dataIndex];
+  alluserList!: getalluser[];
+  getallUser() {
+    this.subsription_get_all_user.add(
+      this._dataService.get_all_user().subscribe(
+        (result) => {
+          if (Array.isArray(result.result)) {
+            this.alluserList = result.result;
+            this.alluserList.forEach((user)=>{
+              user.account_type = Number(user.account_type);
+              user.accountTypeName = this.getAccountType(user.account_type);
+            })
+            if (this.paginator && this.sort) {
+              this.alluserData = new MatTableDataSource(this.alluserList);
+              this.alluserData.paginator = this.paginator;
+              this.alluserData.sort = this.sort;
+            }
           }
         },
-      },
+        (error) => {
+          console.log(error);
+        }
+      )
+    );
+  }
+  accountTypeText : string = ''
+
+  accountTypeName : any [] = [
+    { value : 1 ,
+      text : 'Admin'
     },
-  };
-  public pieChartData: ChartData<'pie', number[], string | string[]> = {
-    labels: ['Continue College', 'Beneficiaries'],
-    datasets: [
-      {
-        data: [],
-      },
-    ],
-  };
-  public pieChartType: ChartType = 'pie';
-  public pieChartPlugins = [DataLabelsPlugin];
+    {
+      value : 2,
+      text : '4ps Staff'
+    },
+    {
+      value : 3,
+      text : 'Beneficiary'
+    },
+    {
+      value : 4,
+      text: 'School Registrar'
+    }
+  ];
+
+  getAccountType(account_type: number): string {
+    const status = this.accountTypeName.find(
+      (option) => option.value === account_type
+    );
+    return status ? status.text : '';
+  }
 }
